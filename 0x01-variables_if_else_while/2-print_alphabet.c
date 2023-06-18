1196c87079575ebe5154a4de8082e63133f7e99a
G3-RA <blessingakp22@gmail.com>
2023-06-18 09:23:54 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints alphabets in lowercase
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i])
+	}
+	putchar('\n');
+	return (0);
+}
