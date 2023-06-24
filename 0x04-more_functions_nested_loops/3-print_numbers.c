0e8ba21de9f2fccaf2e7cbd61d6848e179c45f2b
G3-RA <blessingakp22@gmail.com>
2023-06-23 23:33:44 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numebrs - print numbers 0-9
+ * Return: i
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
